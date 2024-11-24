918c0e43a89adc37d1069d65c115cac7e22f6867
Krayir <krayirnode@gmail.com>
2024-11-24 23:31:35 +0300

    multipl.sh added

---
+++ multipl.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+sleep 1
+echo -e '\e[0;32m'
+echo " _  ______      _ __   _____ ____    _   _  ___  ____  _____ ";
+echo "| |/ /  _ \    / \\ \ / /_ _|  _ \  | \ | |/ _ \|  _ \| ____|";
+echo "| ' /| |_) |  / _ \\ V / | || |_) | |  \| | | | | | | |  _|  ";
+echo "| . \|  _ <  / ___ \| |  | ||  _ <  | |\  | |_| | |_| | |___ ";
+echo "|_|\_\_| \_\/_/   \_\_| |___|_| \_\ |_| \_|\___/|____/|_____|";
+echo ""
+echo -e '\e[0m' 
+sleep 3
+sleep 2
+
+
+echo "System updating..."
+sudo apt update && sudo apt upgrade -y
+
+echo "Downloading..."
+wget https://cdn.app.multiple.cc/client/linux/x64/multipleforlinux.tar -O multipleforlinux.tar
+tar -xvf multipleforlinux.tar
+
+cd multipleforlinux
+
+chmod +x ./multiple-cli
+chmod +x ./multiple-node
+
+echo "Adding directory to system PATH..."
+echo "PATH=\$PATH:$(pwd)" >> ~/.bash_profile
+source ~/.bash_profile
+
+echo "Setting permissions..."
+chmod -R 777 $(pwd)
+
+echo "Launching multiple-node..."
+nohup ./multiple-node > output.log 2>&1 &
+
+echo "Enter identification code and a PIN:"
+read -p "Account ID: " IDENTIFIER
+read -p "Set your PIN: " PIN
+
+echo "Binding account with ID: $IDENTIFIER and PIN: $PIN..."
+multiple-cli bind --bandwidth-download 100 --identifier $IDENTIFIER --pin $PIN --storage 200 --bandwidth-upload 100
+
+echo "Installation complete."
+echo "Krayir Node"
